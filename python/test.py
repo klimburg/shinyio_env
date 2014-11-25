b080a8c70f6d74c2848b01fd186911c33be90555
kevin <limburg.kevin@gmail.com>
2014-11-25 14:59:15 -0600

    modified python code to return python version info

--- python/test.py
+++ python/test.py
@@ -2,4 +2,6 @@
 This is a a test script to see if I can run python from shinyio hosted shiny app
 """
 from datetime import datetime
+import sys
+print "pthon sys. version: " + sys.version
 print datetime.now()
